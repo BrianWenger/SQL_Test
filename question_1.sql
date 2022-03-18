e4ab776f057a23b9d10967534a174b17bcdde547
Brian Wenger <42476640+BrianWenger@users.noreply.github.com>
2022-03-17 20:42:32 -0500

    Update question_1.sql

--- question_1.sql
+++ question_1.sql
@@ -5,3 +5,17 @@ join film f on fa.film_id = f.film_id
 group by 1,2
 order by 3 desc
 LIMIT 100;
+
+/* 
+
+Verified a distinct was not needed on the count, as there do not appear to be duplicated records
+
+SELECT a.first_name, a.last_name, count(distinct f.title)
+FROM actor a
+join film_actor fa on a.actor_id = fa.actor_id
+join film f on fa.film_id = f.film_id
+group by 1,2
+order by 3 desc
+LIMIT 100;
+
+*/
