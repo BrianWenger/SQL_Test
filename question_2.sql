31099b143b44ea748fe3107e883932af88d2cb1b
Brian Wenger <42476640+BrianWenger@users.noreply.github.com>
2022-03-17 20:45:08 -0500

    Update question_2.sql

--- question_2.sql
+++ question_2.sql
@@ -5,3 +5,17 @@ join category c on fc.category_id = c.category_id
 where c.name not in ('Sports', 'Games')
 group by 1
 order by 2 desc;
+
+/* 
+
+As checked on question 1, checking for duplicate records through 'distinct' shows no duplicate records
+
+SELECT c.name as category, count(distinct f.title)
+FROM film f
+join film_category fc on fc.film_id = f.film_id
+join category c on fc.category_id = c.category_id
+where c.name not in ('Sports', 'Games')
+group by 1
+order by 2 desc;
+
+*/
