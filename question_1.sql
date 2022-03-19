ed16522231f247a75d247769fc8919dc307430e7
Brian Wenger <42476640+BrianWenger@users.noreply.github.com>
2022-03-19 16:43:08 -0500

    Update question_1.sql

--- question_1.sql
+++ question_1.sql
@@ -1,4 +1,4 @@
-SELECT a.first_name, a.last_name, count(f.title)
+SELECT a.first_name, a.last_name, count(distinct f.title) as total_films
 FROM actor a
 join film_actor fa on a.actor_id = fa.actor_id
 join film f on fa.film_id = f.film_id
@@ -7,15 +7,5 @@ order by 3 desc
 LIMIT 100;
 
 /* 
-
-Verified a distinct was not needed on the count, as there do not appear to be duplicated records
-
-SELECT a.first_name, a.last_name, count(distinct f.title)
-FROM actor a
-join film_actor fa on a.actor_id = fa.actor_id
-join film f on fa.film_id = f.film_id
-group by 1,2
-order by 3 desc
-LIMIT 100;
-
+Verified there do not appear to be duplicated records. Though the distinct count was don't to ensure no duplicates were counted.
 */
