210876f6fc22b26be48f249c76f2838177eefcb1
Brian Wenger <42476640+BrianWenger@users.noreply.github.com>
2022-03-19 17:19:01 -0500

    Update question_4.sql

--- question_4.sql
+++ question_4.sql
@@ -9,22 +9,32 @@ join inventory i on r.inventory_id = i.inventory_id
 join film f on i.film_id = f.film_id
 WHERE city != 'Dallas'
 and f.rating = 'PG'
+and country_id = 103
 group by 1
 having sum(CASE WHEN date(rental_ts) between (date('07-01-2020') + INTERVAL '1 MONTH' - INTERVAL '15 DAY') and (date('07-01-2020') + INTERVAL '1 MONTH' - INTERVAL '1 DAY') THEN 1 else 0 end) >= 2
 and sum(CASE WHEN date(rental_ts) between (date('08-01-2020') + INTERVAL '1 MONTH' - INTERVAL '15 DAY') and (date('08-01-2020') + INTERVAL '1 MONTH' - INTERVAL '1 DAY') THEN 1 else 0 end) >= 2
-) a
-
-
+) a;
 
 /*
+The question doesn't state to only include US cities, yet the requested columns appear to want to include only US cities. This would be another point
+instance to groom the request with the requestor. The query was narrowed to US only through finding the country_id of the US which is 103. 
 
+The country_id was assumed based on cities in the city table, as the country table was unaccesible.
+
+select country, country_id
+from country
+
+Bad Request
+permission denied for table country
+*/
+
+/*
 Intervals were used in the having clause to allow for quick expansion without the need to count out the days in each month, 
 if for example we were to expand the months to include February (28 days) or September (30 days). 
 The code could have been simplified by hard coding the dates as such:
 
 having sum(CASE WHEN date(rental_ts) between date('07-17-2020') and date('07-31-2020')) >= 2
 and sum(CASE WHEN date(rental_ts) between date('08-17-2020') and date('08-31-2020')) >= 2
-
 */
 
 
