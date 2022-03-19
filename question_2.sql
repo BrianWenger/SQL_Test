a38cdcdd90469acbba35d25ccbcf103691f384ff
Brian Wenger <42476640+BrianWenger@users.noreply.github.com>
2022-03-19 16:44:35 -0500

    Update question_2.sql

--- question_2.sql
+++ question_2.sql
@@ -1,4 +1,4 @@
-SELECT c.name as category, count(f.title)
+SELECT c.name as category, count(distinct f.title) as total_films
 FROM film f
 join film_category fc on fc.film_id = f.film_id
 join category c on fc.category_id = c.category_id
@@ -8,14 +8,7 @@ order by 2 desc;
 
 /* 
 
-As checked on question 1, checking for duplicate records through 'distinct' shows no duplicate records
-
-SELECT c.name as category, count(distinct f.title)
-FROM film f
-join film_category fc on fc.film_id = f.film_id
-join category c on fc.category_id = c.category_id
-where c.name not in ('Sports', 'Games')
-group by 1
-order by 2 desc;
+As checked on question 1, checking for duplicate records through 'distinct' shows no duplicate records. 
+Though the distinct count was don't to ensure no duplicates were counted.
 
 */
